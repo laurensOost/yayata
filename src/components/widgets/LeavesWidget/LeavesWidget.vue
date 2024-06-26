ae0de449eaa196b4bffc7932e60b59318cd20409
NickSettler <moiseevnikita14@gmail.com>
2024-06-26 16:39:37 +0200

    feat(widgets): add selecting year in leaves widget
    add select dropdown with years before the open leave button in the widget header. will be shown and works only if user has multiple leave years

--- src/components/widgets/LeavesWidget/LeavesWidget.vue
+++ src/components/widgets/LeavesWidget/LeavesWidget.vue
@@ -1,14 +1,17 @@
 <script setup>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faArrowUp} from "@fortawesome/free-solid-svg-icons";
 </script>
 
 <template lang="pug">
   div(class='card widget-card shadow-sm' v-if="leaves.length")
-    div(class="card-header widget-px py-0")
+    div(class="card-header justify-content-between widget-px py-0")
       p(class='widget-py')
         | Leaves
-      div
+
+      div(class='d-flex gx-4')
+        select(class='form-control form-control-sm' v-model="year" v-if="hasMultipleLeaveYears")
+          option(v-for="leaveYear in leaveYears" :key="leaveYear" :value="leaveYear") {{ leaveYear }}
         router-link(:to='{ name: "leave" }')
           button(class='btn btn-primary-soft rounded-pill btn-sm btn-square text-primary d-flex align-items-center')
             FontAwesomeIcon(:icon='faArrowUp' :style="{'aspect-ratio': '1/1', transform: 'rotate(45deg)'}" )
@@ -19,7 +22,7 @@ import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
         p Time To
         p Time Action
       div(class="leaves-table")
-        LeavesTableRow(v-for="leave in leaves" :key="leave.id" :leave="leave")
+        LeavesTableRow(v-for="leave in leavesForSelectedYear" :key="leave.id" :leave="leave")
 </template>
 
 <script>
@@ -27,21 +30,80 @@ import LeaveStatusTag from "../../ui/LeaveStatusTag.vue";
 import store from "../../../store";
 import * as types from "../../../store/mutation-types";
 import LeavesTableRow from "./LeavesTableRow.vue";
+import {applyTransforms, transformDate} from "../../../utils/transformation";
+import moment from 'moment';
+import {uniq} from "lodash";
 
 export default {
   name: 'LeavesWidget',
   components: {LeavesTableRow, LeaveStatusTag},
 
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+
   created() {
     store.dispatch(types.NINETOFIVER_RELOAD_LEAVES);
   },
 
+  watch: {
+    /**
+     * Set the year to the latest year in the leave years OR
+     * to the current, if the current year is in the leave years.
+     */
+    leaveYears() {
+      if (this.leaveYears.length === 0) return;
+
+      const currentYear = `${new Date().getFullYear()}`;
+
+      if (this.leaveYears.includes(currentYear)) {
+        this.year = currentYear;
+        return;
+      }
+
+      this.year = `${this.leaveYears.map(Number).sort().reverse()[0]}`;
+    },
+  },
+
   computed: {
+    /** @return {Array<Leave>} */
     leaves() {
       if (!store.getters.leaves) return [];
 
-      return store.getters.leaves
+      return store.getters.leaves.map(applyTransforms(transformDate))
     },
+    /** @return {string[]} */
+    leaveDates() {
+      return uniq(
+          this.leaves.map((leave) =>
+              leave.leavedate_set.map((date) => ([
+                    moment(date.starts_at).format('YYYY-MM-DD'),
+                    moment(date.ends_at).format('YYYY-MM-DD'),
+                  ])
+              ).flat()
+          ).flat()
+      );
+    },
+    /** @return {string[]} */
+    leaveYears() {
+      if (this.leaves.length === 0) return [];
+
+      return uniq(this.leaveDates.map((date) => moment(date).format('YYYY')));
+    },
+    /** @return {Array<Leave>} */
+    leavesForSelectedYear() {
+      return this.leaves.filter((leave) =>
+          leave.leavedate_set.some((date) =>
+              moment(date.starts_at).format('YYYY') === this.year ||
+              moment(date.ends_at).format('YYYY') === this.year
+          )
+      );
+    },
+    hasMultipleLeaveYears() {
+      return this.leaveYears.length > 1;
+    }
   },
 }
 </script>
@@ -57,9 +119,13 @@ p {
   .card-header {
     display: flex;
     align-items: center;
+    max-height: 48px;
+
+    & > div:first-child {
+      width: fit-content;
+    }
 
     & > * {
-      flex-grow: 1;
       display: flex;
 
       &:first-child {
