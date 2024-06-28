3c22f7c93042c612a531d4ea5712c85ef622d180
NickSettler <moiseevnikita14@gmail.com>
2024-06-28 15:24:34 +0200

    refactor(widgets): rewrite contract chart to composition api

--- src/components/widgets/Charts/ContractsChartWidget.vue
+++ src/components/widgets/Charts/ContractsChartWidget.vue
@@ -4,14 +4,117 @@ import {
   Title,
   Tooltip,
   ArcElement,
-  CategoryScale
+  CategoryScale,
 } from 'chart.js'
 import ChartDataLabels from 'chartjs-plugin-datalabels';
+import {computed, ref} from "vue";
+import moment from "moment/moment";
+import store from "../../../store";
+import * as types from "../../../store/mutation-types";
+import Values from "values.js";
+import {isEmpty} from "lodash";
+import {EventBus} from "../../../utils/event-bus";
+import {Doughnut} from "vue-chartjs";
+
+const {timesheet} = defineProps({
+  timesheet: Object
+});
 
 ChartJS.register(Title, Tooltip, ArcElement, CategoryScale, ChartDataLabels);
+
+const inactiveColor = "#EBEBEB";
+const primaryColor = "#0AA6C9";
+
+const contractsData = ref(null);
+const hoverContractIndex = ref(null);
+const options = {
+  responsive: true,
+  onHover: (_, elements) => {
+    hoverContractIndex.value = elements?.[0]?.index;
+  },
+  interaction: {
+    mode: 'point',
+    axis: 'r',
+    intersect: false
+  },
+  hoverOffset: 4,
+  plugins: {
+    tooltip: {
+      callbacks: {
+        label(tooltipItem) {
+          let label = tooltipItem.label.match(/^(\[[\w\/]*])/g)?.[0];
+
+          return isEmpty(label) ? tooltipItem.label : label;
+        },
+        footer: function (tooltipItems) {
+          const item = tooltipItems[0];
+          const caption = [...item?.label.matchAll(/^\[[\w\/]*](.*)/g)]?.[0]?.[1]?.trim();
+
+          return isEmpty(caption) ? item.label : caption;
+        }
+      }
+    }
+  }
+};
+
+const performances = computed(() => contractsData.value?.summary?.performances || []);
+const computedWorkHours = computed(() => (
+    contractsData.value?.work_hours -
+    contractsData.value?.leave_hours -
+    contractsData.value?.holiday_hours
+) ?? 0);
+const performedHours = computed(() => contractsData.value?.performed_hours || 0);
+const chartShades = computed(() => {
+  const count = performances.value.length || 0;
+
+  return new Values(primaryColor).tints(100 / (count + 1)).reverse().slice(1);
+});
+const chartCompleteData = computed(() => ({
+  labels: [
+    ...(performances.value.map(performance => performance.contract.display_label) || []),
+    'Remaining'
+  ],
+  datasets: [{
+    label: "Hours performed",
+    data: [
+      ...(performances.value.map(performance => performance.duration) || []),
+      contractsData.value?.remaining_hours || 0,
+    ],
+    backgroundColor: [...chartShades.value.map(shade => `#${shade.hex}`), inactiveColor],
+    cutout: '80%',
+    datalabels: {
+      anchor: 'center',
+      backgroundColor: null,
+      borderWidth: 0,
+    }
+  }]
+}));
+
+function getData() {
+  let start = moment().year(timesheet.year).month(timesheet.month - 1).startOf('month')
+  let end = moment().year(timesheet.year).month(timesheet.month - 1).endOf('month')
+
+
+  store.dispatch(types.NINETOFIVER_API_REQUEST, {
+    path: '/range_info/',
+    params: {
+      'from': start.format('YYYY-MM-DD'),
+      'until': end.format('YYYY-MM-DD'),
+      'summary': true
+    }
+  }).then(res => {
+    contractsData.value = res.data
+  })
+}
+
+getData()
+
+EventBus.$on('performance:logged', () => {
+  this.getData()
+})
 </script>
 <template lang="pug">
-  div(class='card widget-card widget-px widget-py shadow-sm' v-if="data")
+  div(class='card widget-card widget-px widget-py shadow-sm' v-if="contractsData")
     div(class="card-header mb-3 p-0")
       | Contract summary
 
@@ -34,124 +137,6 @@ ChartJS.register(Title, Tooltip, ArcElement, CategoryScale, ChartDataLabels);
           div(class="legend-dot")
           span {{ performance.contract.display_label }}
 </template>
-
-<script>
-import {Doughnut} from "vue-chartjs";
-import moment from "moment/moment";
-import store from "../../../store";
-import * as types from "../../../store/mutation-types";
-import Values from "values.js";
-import {isEmpty} from "lodash";
-import {EventBus} from "../../../utils/event-bus";
-
-const inactiveColor = "#EBEBEB";
-const primaryColor = "#0AA6C9";
-
-export default {
-  name: 'ContractsChartWidget',
-  props: ['timesheet'],
-  components: {
-    Doughnut
-  },
-  data() {
-    return {
-      data: null,
-      hoverContractIndex: null,
-      options: {
-        responsive: true,
-        onHover: (_, elements) => {
-          this.hoverContractIndex = elements?.[0]?.index;
-        },
-        interaction: {
-          mode: 'point',
-          axis: 'r',
-          intersect: false
-        },
-        hoverOffset: 4,
-        plugins: {
-          tooltip: {
-            callbacks: {
-              label(tooltipItem) {
-                let label = tooltipItem.label.match(/^(\[[\w\/]*])/g)?.[0];
-
-                return isEmpty(label) ? tooltipItem.label : label;
-              },
-              footer: function (tooltipItems) {
-                const item = tooltipItems[0];
-                const caption = [...item?.label.matchAll(/^\[[\w\/]*](.*)/g)]?.[0]?.[1]?.trim();
-
-                return isEmpty(caption) ? item.label : caption;
-              }
-            }
-          }
-        }
-      },
-    }
-  },
-  computed: {
-    performances() {
-      return this.data?.summary?.performances || [];
-    },
-    computedWorkHours() {
-      return (this.data?.work_hours - this.data?.leave_hours - this.data?.holiday_hours) ?? 0;
-    },
-    performedHours() {
-      return this.data?.performed_hours || 0;
-    },
-    chartShades() {
-      const count = this.performances.length || 0;
-
-      return new Values(primaryColor).tints(100 / (count + 1)).reverse().slice(1);
-    },
-    chartCompleteData() {
-      return {
-        labels: [
-          ...(this.performances.map(performance => performance.contract.display_label) || []),
-          'Remaining'
-        ],
-        datasets: [{
-          label: "Hours performed",
-          data: [
-            ...(this.performances.map(performance => performance.duration) || []),
-            this.data?.remaining_hours || 0,
-          ],
-          backgroundColor: [...this.chartShades.map(shade => `#${shade.hex}`), inactiveColor],
-          cutout: '80%',
-          datalabels: {
-            anchor: 'center',
-            backgroundColor: null,
-            borderWidth: 0,
-          }
-        }]
-      }
-    }
-  },
-  created: function () {
-    this.getData()
-
-    EventBus.$on('performance:logged', () => {
-      this.getData()
-    })
-  },
-  methods: {
-    getData() {
-      let start = moment().year(this.timesheet.year).month(this.timesheet.month - 1).startOf('month')
-      let end = moment().year(this.timesheet.year).month(this.timesheet.month - 1).endOf('month')
-
-      store.dispatch(types.NINETOFIVER_API_REQUEST, {
-        path: '/range_info/',
-        params: {
-          'from': start.format('YYYY-MM-DD'),
-          'until': end.format('YYYY-MM-DD'),
-          'summary': true
-        }
-      }).then(res => {
-        this.data = res.data
-      })
-    },
-  }
-}
-</script>
 
 <style scoped lang="scss">
 @import "../../../assets/scss/components/widgets";
