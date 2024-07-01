07529ffae344a145c111b8725da3b132e681abca
NickSettler <moiseevnikita14@gmail.com>
2024-07-01 10:24:07 +0200

    feat(widgets): open leave modal on leaves widget row click

--- src/components/widgets/LeavesWidget/LeavesTableRow.vue
+++ src/components/widgets/LeavesWidget/LeavesTableRow.vue
@@ -1,44 +1,72 @@
+<script setup>
+import LeaveWidget from "../LeaveWidget.vue";
+import LeaveStatusTag from "../../ui/LeaveStatusTag.vue";
+import store from "../../../store";
+import * as types from "../../../store/mutation-types";
+import {computed, ref} from "vue";
+import moment from "moment";
+import {maxBy, minBy} from "lodash";
+
+/**
+ * @typedef {import("vue").Ref} Ref
+ * @typedef {import("bootstrap-vue").BModal} BModal
+ * @typedef {import("../../../store/modules/ninetofiver").Leave} Leave
+ */
+
+/** @type {{ leave: Leave }} */
+const props = defineProps({
+  leave: {
+    type: Object,
+    required: true,
+  },
+});
+
+/** @type {Ref<BModal | null>} */
+const leaveModal = ref(null);
+
+function handleLeaveClick() {
+  if (props.leave.status !== 'pending') return;
+
+  leaveModal.value.show();
+}
+
+function onLeaveModified() {
+  store.dispatch(types.NINETOFIVER_RELOAD_LEAVES);
+
+  leaveModal.value.hide();
+}
+
+const leaveType = computed(() => props.leave?.leave_type?.display_label ?? '');
+const timeFrom = computed(() => {
+  if (!props.leave) return '';
+
+  return moment(minBy(props.leave?.leavedate_set, 'starts_at')?.starts_at)
+      .format(props.leave?.leavedate_set?.length > 1 ? 'DD/MM/YYYY' : 'DD/MM/YYYY HH:mm');
+});
+const timeTo = computed(() => {
+  if (!props.leave) return '';
+
+  return moment(maxBy(props.leave?.leavedate_set, 'ends_at')?.ends_at)
+      .format(props.leave?.leavedate_set?.length > 1 ? 'DD/MM/YYYY' : 'DD/MM/YYYY HH:mm');
+});
+</script>
 <template lang="pug">
-  div(class="leaves-row")
+  div(
+    class="leaves-row selectable"
+    :class="{'selectable': leave.status === 'pending'}"
+    @click="handleLeaveClick()"
+  )
     p {{ leaveType }}
     p {{ timeFrom }}
     p {{ timeTo }}
     LeaveStatusTag(v-if='leave.status' :status="leave.status")
+
+    b-modal(ref='leaveModal' hide-header=true hide-footer=true lazy=true size='lg' content-class="bg-transparent border-0")
+      LeaveWidget(
+        :leave='leave'
+        @success="onLeaveModified()"
+      )
 </template>
-
-<script>
-import LeaveStatusTag from "../../ui/LeaveStatusTag.vue";
-import {maxBy, minBy} from "lodash";
-
-export default {
-  name: 'LeavesTableRow',
-  components: {LeaveStatusTag},
-
-  props: {
-    leave: {
-      type: Object,
-      required: true,
-    }
-  },
-
-  computed: {
-    leaveType() {
-      return this.leave?.leave_type?.display_label ?? '';
-    },
-    timeFrom() {
-      if (!this.leave) return '';
-
-      return moment(minBy(this.leave?.leavedate_set, 'starts_at')?.starts_at).format(this.leave?.leavedate_set?.length > 1 ? 'DD/MM/YYYY' : 'DD/MM/YYYY HH:mm');
-    },
-    timeTo() {
-      if (!this.leave) return '';
-
-      return moment(maxBy(this.leave?.leavedate_set, 'ends_at')?.ends_at).format(this.leave?.leavedate_set?.length > 1 ? 'DD/MM/YYYY' : 'DD/MM/YYYY HH:mm');
-    },
-  },
-}
-
-</script>
 
 <style scoped lang="scss">
 @import "../../../assets/scss/components/widgets";
@@ -49,6 +77,13 @@ export default {
   align-items: center;
   padding: 8px 22px;
   border-bottom: 1px solid $gray-400;
+
+  &.selectable {
+    &:hover {
+      cursor: pointer;
+      background: $gray-100;
+    }
+  }
 
   p {
     font-size: 14px;
